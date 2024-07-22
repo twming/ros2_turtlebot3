aacf112ea58c7552fa1cce4278b44beaa08bc216
twming <tanwoeiming@gmail.com>
2024-07-22 21:08:22 +0800

    Update braccio_ros2.ino

--- ArduinoCode/braccio_ros2/braccio_ros2.ino
+++ ArduinoCode/braccio_ros2/braccio_ros2.ino
@@ -7,21 +7,22 @@ Servo elbow;
 Servo wrist_ver;
 Servo wrist_rot;
 Servo gripper;
-unsigned int _baseAngle = 90;
-unsigned int _shoulderAngle = 90;
-unsigned int _elbowAngle = 90;
-unsigned int _wrist_verAngle = 90;
-unsigned int _wrist_rotAngle = 90;
-unsigned int _gripperAngle =120; //closed
+int _baseAngle = 20;
+int _shoulderAngle = 90;
+int _elbowAngle = 90;
+int _wrist_verAngle = 90;
+int _wrist_rotAngle = 90;
+int _gripperAngle =73; //closed
 
-const int BUFFER_SIZE = 6;
-char buf[BUFFER_SIZE]={90,90,90,90,90,120};
+const int BUFFER_SIZE = 12;
+byte buf[BUFFER_SIZE]={20,90,90,90,90,73};
 
 void setup()
 { 
   //Initialzie the Braccio Arm and Serial Port : Bit rate 115200 to match with ROS2 default
   Braccio.begin();
   Serial.begin(115200);
+  Braccio.ServoMovement(20,_baseAngle,_shoulderAngle,_elbowAngle,_wrist_verAngle,_wrist_rotAngle,_gripperAngle);
 }
 
 void loop()
@@ -31,12 +32,12 @@ void loop()
   {
       //Read data from Serial port and assign to buf memory
       Serial.readBytes(buf, BUFFER_SIZE);
-      _baseAngle      =buf[0];
-      _shoulderAngle  =buf[1];
-      _elbowAngle     =buf[2];
-      _wrist_verAngle =buf[3];
-      _wrist_rotAngle =buf[4];
-      _gripperAngle   =buf[5];
+      _baseAngle      = (int(0)|buf[0]) <<8 | buf[1];
+      _shoulderAngle  = (int(0)|buf[2]) <<8 | buf[3];
+      _elbowAngle     = (int(0)|buf[4]) <<8 | buf[5];
+      _wrist_verAngle = (int(0)|buf[6]) <<8 | buf[7];
+      _wrist_rotAngle = (int(0)|buf[8]) <<8 | buf[9];
+      _gripperAngle   = (int(0)|buf[10]) <<8 | buf[11];
 
       //Move the Braccio Arm with the input angles
       Braccio.ServoMovement(20,_baseAngle,_shoulderAngle,_elbowAngle,_wrist_verAngle,_wrist_rotAngle,_gripperAngle);
@@ -46,4 +47,3 @@ void loop()
   //delay(200);
   
 }
-
