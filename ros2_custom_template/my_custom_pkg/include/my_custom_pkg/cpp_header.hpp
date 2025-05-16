db10a2ae20fc2ca7b47042ab7367f5b8518c6d06
twming <tanwoeiming@gmail.com>
2025-05-16 18:45:04 +0800

    commit

--- ros2_custom_template/my_custom_pkg/include/my_custom_pkg/cpp_header.hpp
+++
@@ -1,9 +0,0 @@
-#include "rclcpp/rclcpp.hpp"
-
-class MyCustomNode : public rclcpp::Node
-{
-    public:
-        MyCustomNode(): Node("my_node") {
-            RCLCPP_INFO(this->get_logger(),"TEST Cpp Node");
-        }
-};
