b01471527c25f5565576a70b8d594da519264b0e
twming <tanwoeiming@gmail.com>
2025-05-16 21:38:49 +0800

    commit

--- src/braccio_arm_moveit/build/braccio_arm_moveit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/twm/braccio_arm_moveit
-# Build directory: /home/twm/braccio_arm_moveit/build/braccio_arm_moveit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
